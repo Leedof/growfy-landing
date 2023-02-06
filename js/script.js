9490362b200f4afb541ccd3b299c698dfdef00f2
Leedof <m4kushchenko@gmail.com>
2023-02-06 12:23:59 +0200

    Adaptive Header section was made and was styled.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+document.addEventListener("click", onClickHandler);
+
+function onClickHandler(e) {
+  const target = e.target;
+
+  if (target.closest(".icon-menu")) {
+    document.documentElement.classList.toggle("menu-open");
+  }
+}
